4361f6f0aa19a09c5e777c0a213129a0dd34e907
aniketdhole7 <40766879+aniketdhole07@users.noreply.github.com>
2020-10-09 19:17:42 +0530

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,12 @@
+from os import path
+from pydub import AudioSegment
+import os
+import sys
+
+count=1
+for file in [doc for doc in os.listdir(os.curdir) if doc.endswith(".mp3")]:
+	print(file)     
+	dst="GD_roaring."+str(count)+".wav"                                                          
+	sound = AudioSegment.from_mp3(file)
+	sound.export(dst, format="wav")
+	count+=1
